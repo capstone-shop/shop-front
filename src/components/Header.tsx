779fa3cdf27ea93a222438523fa30657824a0152
컴퓨터\kim-jin-young <wlsdud11457@gmail.com>
2024-11-02 16:51:38 +0900

    init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/css/Header.css';
 import { Link } from 'react-router-dom';
+
 function Header() {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  // 카테고리 목록 데이터 예시
+  const categories = [
+    {
+      name: '채소',
+      subcategories: [
+        '친환경',
+        '고구마·감자·당근',
+        '시금치·쌈채소·나물',
+        '브로콜리·피프리카·양배추',
+        '양파·대파·마늘·배추',
+      ],
+    },
+    {
+      name: '과일·견과·쌀',
+      subcategories: [
+        '제철과일',
+        '국산과일',
+        '수입과일',
+        '간편과일',
+        '냉동·건과일',
+      ],
+    },
+    {
+      name: '수산·해산·건어물',
+      subcategories: ['생선류', '조개류', '오징어·낙지·문어', '건어물·해조류'],
+    },
+    {
+      name: '정육·가공육·계란',
+      subcategories: ['소고기', '돼지고기', '닭고기', '계란류', '양고기'],
+    },
+    {
+      name: '국·반찬·메인요리',
+      subcategories: [
+        '국/탕/찌개',
+        '밑반찬',
+        '김치/젓갈/장류',
+        '두부/어묵/부침개',
+        '메인요리',
+      ],
+    },
+    {
+      name: '간편식·밀키트·샐러드',
+      subcategories: ['간편식', '밀키트', '샐러드', '도시락', '즉석밥/죽'],
+    },
+    {
+      name: '면·양념·오일',
+      subcategories: ['면류', '파스타', '양념', '소스/드레싱', '식용유/오일'],
+    },
+    {
+      name: '생수·음료',
+      subcategories: ['생수/탄산수', '주스', '탄산음료', '커피', '차류'],
+    },
+    {
+      name: '베이커리·간식',
+      subcategories: ['빵류', '케이크', '과자/스낵', '초콜릿', '떡/한과'],
+    },
+    {
+      name: '유제품·아이스크림',
+      subcategories: ['우유', '치즈', '버터/마가린', '요거트', '아이스크림'],
+    },
+  ];
+
   return (
-    <div className={'div0'}>
-      <div className={'div1'}>
-        <div className={'div1-1'}>
-          <img src={''} alt={'회사 로고'} />
-          <Link className={'form-link'} to="/">
+    <div className="div0">
+      <div className="div1">
+        <div className="div1-1">
+          <img src={''} alt="회사 로고" />
+          <Link className="form-link" to="/">
             <span>회사이름</span>
           </Link>
         </div>
-        <div className={'div1-2'}>
+        <div className="div1-2">
           <input
-            className={'search-header-input'}
+            className="search-header-input"
             type="text"
-            placeholder={'검색어를 입력해주세요'}
+            placeholder="검색어를 입력해주세요"
           />
-          <button className={'search-header-btn'}>
+          <button className="search-header-btn">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -30,41 +96,126 @@ function Header() {
             </svg>
           </button>
         </div>
-        <div className={'div1-3'}>
-          <Link className={'form-link'} to="/signUp">
+        <div className="div1-3">
+          <Link className="form-link" to="/signUp">
             <span>회원가입</span>
           </Link>
-          <Link className={'form-link'} to="/signIn">
+          <Link className="form-link" to="/signIn">
             <span>로그인</span>
           </Link>
         </div>
       </div>
-      <div className={'div2'}>
-        <div>
+      <div className="div2">
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            fontSize: '12px',
+            fontWeight: '550',
+            position: 'relative',
+            cursor: 'pointer',
+          }}
+          onMouseEnter={() => setDropdownVisible(true)}
+          onMouseLeave={() => setDropdownVisible(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 24"
+            fill="currentColor"
+            aria-label="Hamburger menu icon"
+            style={{ marginRight: '10px' }}
+          >
+            <rect y="3" width="18" height="2" rx="1" />
+            <rect y="10" width="18" height="2" rx="1" />
+            <rect y="17" width="18" height="2" rx="1" />
+          </svg>
           <span>카테고리</span>
+
+          {isDropdownVisible && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                display: 'flex',
+                backgroundColor: 'white',
+                border: '1px solid #ccc',
+                boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)',
+                width: '400px',
+                zIndex: 1,
+              }}
+              onMouseEnter={() => setDropdownVisible(true)}
+              onMouseLeave={() => setDropdownVisible(false)} // 드롭다운 전체 영역에서 마우스가 벗어날 때 닫힘
+            >
+              {/* 왼쪽 카테고리 목록 */}
+              <div
+                style={{
+                  width: '50%',
+                  maxHeight: '300px',
+                  overflowY: 'auto',
+                  borderRight: '1px solid #ccc',
+                }}
+              >
+                {categories.map((category, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      padding: '8px 16px',
+                      fontSize: '14px',
+                      color:
+                        selectedCategory === category.name ? '#5c2ea6' : '#333',
+                      backgroundColor:
+                        selectedCategory === category.name
+                          ? '#f5f5f5'
+                          : 'transparent',
+                      cursor: 'pointer',
+                    }}
+                    onMouseEnter={() => setSelectedCategory(category.name)}
+                  >
+                    {category.name}
+                  </div>
+                ))}
+              </div>
+
+              {/* 오른쪽 하위 카테고리 목록 */}
+              <div
+                style={{
+                  width: '50%',
+                  maxHeight: '300px',
+                  overflowY: 'auto',
+                  padding: '8px 16px',
+                }}
+              >
+                {selectedCategory &&
+                  categories
+                    .find((category) => category.name === selectedCategory)
+                    ?.subcategories.map((subcategory, index) => (
+                      <div
+                        key={index}
+                        style={{
+                          padding: '4px 0',
+                          fontSize: '14px',
+                          color: '#333',
+                          cursor: 'pointer',
+                        }}
+                        onMouseEnter={(e) => {
+                          const target = e.currentTarget as HTMLDivElement;
+                          target.style.backgroundColor = '#f0f0f0';
+                        }}
+                        onMouseLeave={(e) => {
+                          const target = e.currentTarget as HTMLDivElement;
+                          target.style.backgroundColor = 'transparent';
+                        }}
+                      >
+                        {subcategory}
+                      </div>
+                    ))}
+              </div>
+            </div>
+          )}
         </div>
-        {/*<div>*/}
-        {/*  <li>*/}
-        {/*    <span>*/}
-        {/*      <a href={''}>목록 1</a>*/}
-        {/*    </span>*/}
-        {/*  </li>*/}
-        {/*  <li>*/}
-        {/*    <span>*/}
-        {/*      <a href={''}>목록 2</a>*/}
-        {/*    </span>*/}
-        {/*  </li>*/}
-        {/*  <li>*/}
-        {/*    <span>*/}
-        {/*      <a href={''}>목록 3</a>*/}
-        {/*    </span>*/}
-        {/*  </li>*/}
-        {/*  <li>*/}
-        {/*    <span>*/}
-        {/*      <a href={''}>목록 4</a>*/}
-        {/*    </span>*/}
-        {/*  </li>*/}
-        {/*</div>*/}
       </div>
     </div>
   );
